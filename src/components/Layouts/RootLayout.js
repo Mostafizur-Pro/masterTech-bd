d241d756081b435ef8cc227e282c9aad1e675a5f
Mostafizur-Pro <mostafizur0195@gmail.com>
2023-07-31 11:18:56 +0600

    navber & error page add

--- src/components/Layouts/RootLayout.js
+++ src/components/Layouts/RootLayout.js
@@ -1,8 +1,12 @@
 import Footer from "../shared/Footer";
+import Navbar from "../shared/Navber";
+import Sidebar from "../shared/Sidebar";
 
 function RootLayout({ children }) {
   return (
     <div>
+      <Navbar />
+      <Sidebar />
       {children}
       <Footer />
     </div>
